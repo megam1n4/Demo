18dc39f4bf419b9d2f3cdce653173fc3d9add033
Rahul Debnath <rdebnath086@gmail.com>
2020-01-13 00:53:26 +0600

    RelativeLayout Button Action Toast Activity lifecycle callback methods are added.

--- app/src/main/java/com/demo/megam1n4/MainActivity.java
+++ app/src/main/java/com/demo/megam1n4/MainActivity.java
@@ -1,14 +1,69 @@
 package com.demo.megam1n4;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toast.makeText(this, "onCreate called", Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.et1);
+        button=findViewById(R.id.btn1);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sms=editText.getText().toString();
+                Toast.makeText(MainActivity.this, sms, Toast.LENGTH_SHORT).show();   //Toast +Tab
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onReStart called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy called", Toast.LENGTH_SHORT).show();
     }
 }
