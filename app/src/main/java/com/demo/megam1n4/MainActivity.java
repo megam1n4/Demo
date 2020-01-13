f88fc40c961e1f8a6fdfadad381cb33b44cd0af7
Rahul Debnath <rdebnath086@gmail.com>
2020-01-14 02:08:06 +0600

    Changed to Constraint Layout

--- app/src/main/java/com/demo/megam1n4/MainActivity.java
+++ app/src/main/java/com/demo/megam1n4/MainActivity.java
@@ -2,68 +2,63 @@ package com.demo.megam1n4;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
-    private Button button;
+public class MainActivity extends AppCompatActivity
+{
+    int randomNumber;
+
+    public void generateRandomNumber()
+    {
+        Random rand = new Random();
+
+        randomNumber = rand.nextInt(20)+1;
+    }
+
+    public void guess(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        int guessValue = Integer.parseInt(editText.getText().toString());
+
+        String message;
+
+        if(guessValue > randomNumber)
+        {
+            message = "Lower!";
+        }
+
+        else if(guessValue<randomNumber)
+        {
+            message = "Higher!";
+        }
+
+        else
+        {
+            message = "You Got It! Try again & again Bitch ><!";
+
+            generateRandomNumber();
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        Log.i("Entered Value", editText.getText().toString());
+
+        Log.i("Random Number" , Integer.toString(randomNumber));
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast.makeText(this, "onCreate called", Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText=findViewById(R.id.et1);
-        button=findViewById(R.id.btn1);
 
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String sms=editText.getText().toString();
-                Toast.makeText(MainActivity.this, sms, Toast.LENGTH_SHORT).show();   //Toast +Tab
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStart called", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume called", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause called", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop called", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "onReStart called", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy called", Toast.LENGTH_SHORT).show();
+        generateRandomNumber();
     }
 }
